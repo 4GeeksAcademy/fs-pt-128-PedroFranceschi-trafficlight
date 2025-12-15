0ebb64ec8d1beac0b6067f8babab23fcb638cb1b
Pedro Franceschi <117317386+FranceschiPedro@users.noreply.github.com>
2025-12-15 15:30:58 +0000

    Primera Version

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [activeLight, setActiveLight] = useState("verde");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const glow = (color) => {
+		activeLight == color ? { 
+			boxShadow:`0 0 10px ${color},
+		 			   0 0 20px ${color},
+					   0 0 40px ${color}`,
+		} : {};
+	}
+
+	return (
+		<div className="mt-5">
+			<div className="semaforo">
+				<div
+					id="verde"
+					style={glow("green")}
+					className={`luz ${activeLight === "verde" ? "active" : ""}`}
+					onClick={() => setActiveLight("verde")}
+				></div>
+
+				<div
+					id="amarillo"
+					style={glow("yellow")}
+					className={`luz ${activeLight === "amarillo" ? "active" : ""}`}
+					onClick={() => setActiveLight("amarillo")}
+				></div>
+
+				<div
+					id="rojo"
+					style={glow("red")}
+					className={`luz ${activeLight === "rojo" ? "active" : ""}`}
+					onClick={() => setActiveLight("rojo")}
+				></div>
+			</div>
 		</div>
 	);
 };
